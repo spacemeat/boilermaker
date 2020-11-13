f5173c8679ec7f92baa485ac834fd24f412ba68c
Trevor Schrock <spacemeat@gmail.com>
2020-11-13 11:02:22 -0800

    Small build script fix.

--- buildSample.sh
+++ buildSample.sh
@@ -1,5 +1,5 @@
 shopt -s nullglob
-if [ $1 != '-ho' ]; then
+if [ "${1}." != '-ho.' ]; then
     echo "Making build/bin/sample using generated .cpp source."
     srcFiles=(./sample/gen-cpp/src/*.cpp)
     g++ -std=c++17 -Wall -O0 -g -Wextra sample/sample.cpp ${srcFiles[@]} -Isample -o build/bin/sample -lhumon
